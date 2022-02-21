e9f6800abb86f4302cfb22dc79ed9ed117dd74fb
joel-samuel-raj <80976002+joel-samuel-raj@users.noreply.github.com>
2022-02-22 02:29:40 +0530

    asdfasdf

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,9 @@ async function bootstrap() {
   }))
   app.use(passport.initialize())
   app.use(passport.session())
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, () => {
+    console.log('Hello World')
+  });
 }
 bootstrap(); 
   
